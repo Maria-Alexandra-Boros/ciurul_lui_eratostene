02819063fa7e659ac9166d00f7339fa123093f8f
Maria Alexandra <inlumeamariei@gmail.com>
2022-03-01 22:22:17 +0200

    Initial Commit

---
+++ ciurul_lui_eratostene/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ciurul_lui_eratostene
+//
+//  Created by Teodora on 01.03.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
